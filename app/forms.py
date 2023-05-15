a802a7d033ba0934a269dd29c00b36ca3882292d
Lamecksdg <kenyambilameck@gmail.com>
2023-05-15 10:26:55 +0300

    git
    Signed-off-by: Lamecksdg <kenyambilameck@gmail.com>

--- app/forms.py
+++ app/forms.py
@@ -1,5 +1,5 @@
 from django import forms
-from django.contrib.auth.forms import UserCreationForm, AuthenticationForm, UsernameField, PasswordChangeForm, SetPasswordForm
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm, UsernameField, PasswordChangeForm, SetPasswordForm, PasswordResetForm
 from django.contrib.auth.forms import User
 
 from .models import Customer
@@ -24,10 +24,10 @@ class MyPasswordChangeForm(PasswordChangeForm):
     new_password1 =forms.CharField(label='New Password', widget=forms.PasswordInput(attrs={'autofocus ':'True','autocomplete':'current-password','class':'form-control'}))
     new_password2=forms.CharField(label='Confirm Password', widget=forms.PasswordInput(attrs={'autofocus ':'True','autocomplete':'current-password','class':'form-control'}))
 
-class MyPasswordResetForm(PasswordChangeForm):
+class MyPasswordResetForm(PasswordResetForm):
     email = forms.EmailField(widget=forms.EmailInput(attrs={'class':'form-controlu'}))
 
-class MySetPassword(SetPasswordForm):
+class MySetPasswordForm(SetPasswordForm):
     new_password1 =forms.CharField(label='New Password', widget=forms.PasswordInput(attrs={'autofocus ':'True','autocomplete':'current-password','class':'form-control'}))
     new_password2=forms.CharField(label='Confirm New Password', widget=forms.PasswordInput(attrs={'autofocus ':'True','autocomplete':'current-password','class':'form-control'}))
 
